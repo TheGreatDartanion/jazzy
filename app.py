6a72f687426a5ba066a264f3f93646c2f1f7ca98
Williams, Dartanion De Angelo <dwilliams@bootcampspot.com>
2020-08-06 03:41:26 -0400

    take 77

--- app.py
+++ app.py
@@ -8,11 +8,6 @@ import bs4
 import json
 import pymysql
 
-remote_db_endpoint = 'sba-proj-2.cua1jo4mglzx.us-east-2.rds.amazonaws.com'
-remote_db_port = '3306'
-remote_db_user = 'admin'
-remote_db_pwd = 'ZXecee*8aU-'
-remote_db_name ='sba-schema'
 
 
 pymysql.install_as_MySQLdb()
